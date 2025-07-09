c0cdd279c7313fe1f751b01e3e298950cb6592af
Arie Rahmadi <arie.rmd@gmai.com>
2025-07-09 16:05:40 +0900

    change implementasi UI React + Tailwind

--- JobSheet/ui-react-tailwind/src/components/Navbar.jsx
+++ JobSheet/ui-react-tailwind/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ function Navbar({ onOpenLogin, onOpenRegister }) {
   return (
     <nav className="bg-blue-600 text-white shadow-md">
       <div className="max-w-6xl mx-auto px-4 py-3 flex justify-between items-center">
-        <h1 className="font-bold text-lg">Menu Program</h1>
+        <h1 className="font-bold text-lg">Job Sheet</h1>
         <ul className="flex gap-6">
           <li><Link to="/" className="hover:underline">Beranda</Link></li>
           <li><Link to="/peserta" className="hover:underline">Data Peserta</Link></li>
