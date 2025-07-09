8c57d59181051cf9cbaac799d887e0a7d7ae6dd0
Arie Rahmadi <arie.rmd@gmai.com>
2025-07-09 10:59:12 +0900

    Example Menu Program use React + Tailwind

---
+++ Menerapkan-Menu-Program-Sesuai-Rancangan/React-Router-Tailwind/src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav className="bg-blue-600 text-white shadow-md">
+      <div className="max-w-6xl mx-auto px-4 py-3 flex justify-between items-center">
+        <h1 className="font-bold text-lg">Menu Program</h1>
+        <ul className="flex gap-6">
+          <li><Link to="/" className="hover:underline">Beranda</Link></li>
+          <li><Link to="/peserta" className="hover:underline">Data Peserta</Link></li>
+          <li><Link to="/laporan" className="hover:underline">Laporan</Link></li>
+          <li><Link to="/logout" className="hover:underline">Logout</Link></li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
+ 
